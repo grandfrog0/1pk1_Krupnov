e2ebd67ff1a935232667ef8f9c8ec2112b9cccdb
Maxim Krupnov <maksspecial32@gmail.com>
2025-01-20 11:09:51 +0500

    выполнена 2 задача 2 урока

--- Task_02_02/Program.cs
+++ Task_02_02/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!!");
+            int a = 8;
+            int b = 14;
+            double c = Math.PI / 4;
+
+            double step1 = Math.Pow(b + Math.Pow(a - 1, 1.0 / 3.0), 1.0 / 4.0);
+            double step2 = Math.Abs(a - b) * (Math.Pow(Math.Sin(c), 2) + Math.Tan(c));
+            double step3 = step1 / step2;
+
+            Console.WriteLine(step3);
         }
     }
 }
