be9896b8399cd61a5292cd5293fc02758bef0e1a
Maxim Krupnov <maksspecial32@gmail.com>
2025-01-22 20:18:37 +0500

    выполнена 4 задача 3 урока

--- Task_03_04/Program.cs
+++ Task_03_04/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace Task_03_04
-{
+{   
     internal class Program
     {
+        /*
+        Пользователь вводить в консоли произвольный текст.
+        После каждого ввода консоль очищается. Когда пользователь вводит
+        слово «exit» или пустую строку – ввод останавливается и
+        выводиться количество строк, введенных пользователем.
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int count = 0;
+            while (true)
+            {
+                string text = Console.ReadLine();
+                Console.Clear();
+                if (text == "exit" || text == "") break;
+                count++;
+            }
+            Console.WriteLine("Введено произвольных строк: " + count);
         }
     }
 }
