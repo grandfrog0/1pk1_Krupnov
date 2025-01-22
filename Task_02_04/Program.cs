d4f18ac8adb0610d8c7ce8ce6a21449f4f977f2e
Maxim Krupnov <maksspecial32@gmail.com>
2025-01-22 19:53:26 +0500

    выполнена 4 задача 2 урока

--- Task_02_04/Program.cs
+++ Task_02_04/Program.cs
@@ -1,19 +1,25 @@
-﻿namespace Task_02_04
+﻿using System.Data;
+
+namespace Task_02_04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите год рождения:");
             int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц рождения:");
             int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите день рождения:");
             int day = int.Parse(Console.ReadLine());
 
-            int delta_day = 20 - day;
-            int delta_month = 1 - month;
+            DateTime today = DateTime.Now;
+            int delta_day = today.Day - day;
+            int delta_month = today.Month - month;
             if (delta_day < 0) delta_month--;
-            int delta_year = 2025 - year;
+            int delta_year = today.Year - year;
             if (delta_month < 0) delta_year--;
-            Console.WriteLine(delta_year >= 18);
+            Console.WriteLine(delta_year >= 18 ? "Пользователь является совершеннолетним" : "Пользователь не является совершеннолетним");
         }
     }
 }
