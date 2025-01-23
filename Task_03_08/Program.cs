50f5b040a1d8799c34bc9a324d861b53375b4c86
Maxim Krupnov <maksspecial32@gmail.com>
2025-01-23 17:40:57 +0500

    выполнена 8 задача 3 урока

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        //Даны натуральные числа от 20 до 50. Напечатать те из них, которые делятся на 3, но не делятся на 5.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Натуральные числа от 20 до 50, кратные 3 и не кратные 5:");
+            for (int i = 20; i <= 50; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0) Console.WriteLine(i);
+            }
+        }
+    }
+}
