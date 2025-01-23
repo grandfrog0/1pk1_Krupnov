311d979a231f199987415e43912b682b1a7287cd
Maxim Krupnov <maksspecial32@gmail.com>
2025-01-23 17:37:16 +0500

    выполнена 7 задача 3 урока

--- Task_03_07/Program.cs
+++ Task_03_07/Program.cs
@@ -3,23 +3,24 @@
     internal class Program
     {
         /*
-        Написать программу, которая выводит таблицу скорости(через каждые 0,5с)
-        свободно падающего тела v = g*t, где g = 9,8 м/с2 – ускорение свободного падения.
+        Написать программу, которая выводит таблицу скорости (через каждые 0,5с)
+        свободно падающего тела v = g * t, где g = 9,8 м/с2 – ускорение свободного падения.
         */
         static void Main(string[] args)
         {
             Console.WriteLine("Скорость свободно падающего тела");
-            Console.WriteLine("  t  |  v  ");
+            Console.WriteLine("Введите время окончания (с)");
+            int t_max = Int32.Parse(Console.ReadLine());
 
-            float min = -4.3333f, max = 4;
-            float h = 0.5f;
-            for (float x = min; x <= max; x += h)
+            float delta_time = 0.5f;
+            float g = 9.8f;
+            for (float t = 0; t < t_max; t += delta_time)
             {
-                double rounded = Math.Round(x * 100) / 100; //Округляем X до сотых
-                string text = rounded.ToString() + " ";
-                if (text.Length < 6) for (int j = 0; j < 6 - text.Length; j++) text = text.Insert(0, " "); // Выравниваем текст пробелами слева
-                text += Math.Abs(rounded);
-                Console.WriteLine(text);
+                double v = g * t;
+                v = Math.Round(v * 100) / 100;
+                Console.Write($"t = {t}с; ");
+                for (int i = 0; i < 10 - t.ToString().Length; i++) Console.Write(" ");
+                Console.WriteLine($"v = {v}м/с");
             }
         }
     }
