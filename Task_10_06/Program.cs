ec94d7aaa231f1f04f1d16a74ee133001d80598a
grandfrog0 <maksspecial32@gmail.com>
2025-02-15 12:01:38 +0500

    выполнена 6 задача 10 урока

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    {
+        //Создать метод ArrayGeneration, не возвращающий значения, который принимает целое число n, 
+        //выводит на консоль сгенерированный массив размерности n*n.
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Узнаем число n у пользователя
+                Console.WriteLine("Введите размерность массива n: ");
+                string text = Console.ReadLine();
+                int n = -1;
+                while (!int.TryParse(text, out n) || n <= 0)
+                {
+                    Console.WriteLine("Введите число больше нуля.");
+                    text = Console.ReadLine();
+                }
+
+                ArrayGeneration(n);
+            }
+        }
+
+        static void ArrayGeneration(int n)
+        {
+            // Создание массива, заполнение случайными значениями
+            Random rnd = new Random();
+            int[,] arr = new int[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] = rnd.Next(0, 100);
+                }
+
+            // Вывод массива
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    string add = "";
+                    for (int k = 0; k < 3 - arr[i, j].ToString().Length; k++) add += "0";
+                    Console.Write(add + arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
