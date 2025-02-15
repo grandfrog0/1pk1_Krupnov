4ea947e499ca3b218bedaebc027bbc2bd9c74900
grandfrog0 <maksspecial32@gmail.com>
2025-02-15 17:52:31 +0500

    выполнена 4 задача 11 урока

---
+++ Task_11_04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_11_04
+{
+    internal class Program
+    {
+        //Массив параметров (params): Напишите метод, который принимает массив чисел и возвращает
+        //их среднее значение.Используйте ключевое слово params
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Average(5,4,2,0,0,123, 0.213, 0));
+        }
+
+        /// <summary>
+        /// Возвращает среднее арифметическое чисел (округляется до 3 знаков после запятой)
+        /// </summary>
+        /// <param name="x">Массив переданных чисел</param>
+        /// <returns>double</returns>
+        static double Average(params double[] x)
+        {
+            double sum = 0;
+            foreach (var el in x) sum += el;
+
+            return Math.Round(sum / x.Length, 3);
+        }
+    }
+}
