54b472ba3a0742182966a642dc0210ee3cf2cda4
grandfrog0 <maksspecial32@gmail.com>
2025-02-15 13:06:57 +0500

    выполнена 1 задача 11 урока

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_11_01
+{
+    //Передача по значению: Напишите метод, который принимает два целых числа и меняет их
+    //местами. Проверьте, изменились ли значения переменных вне метода
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+
+            Console.WriteLine($"a до преобразований равен {a}, b равен {b}.");
+
+            Swap(a, b);
+
+            Console.WriteLine($"a после преобразований все ещё равен {a}, b также равен {b}.");
+        }
+
+        /// <summary>
+        /// Локально меняет значения местами
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void Swap(int a, int b)
+        {
+            int val = a;
+            a = b;
+            b = val;
+
+            Console.WriteLine($"Вызов метода Swap: a внутри метода равен {a}, b равен {b}.");
+        }
+    }
+}
