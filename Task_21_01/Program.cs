c09ec035cb0f5f3466070995d056cf1b9293720f
grandfrog0 <maksspecial32@gmail.com>
2025-05-03 16:55:56 +0500

    выполнены 1, 2 задачи 21 урока

---
+++ Task_21_01/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_21_01
+{
+    // Дан список чисел. Создать новый список,
+    // содержащий только чётные числа из исходного списка, умноженные на 10.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину списка: ");
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n) && n > 0)
+                Console.Write("Введите корректную длину списка! ");
+
+            List<int> list1 = GenerateList(n);
+            Console.WriteLine($"Первоначальный список: {string.Join(", ", list1)}");
+
+            List<int> list2 = new();
+
+            foreach (int el in list1)
+                if (el % 2 == 0)
+                    list2.Add(el * 10);
+
+            Console.WriteLine($"Новый список: {string.Join(", ", list2)}");
+        }
+
+        static List<int> GenerateList(int length)
+        {
+            List<int> list = new();
+            Random rnd = new();
+            for (int i = 0; i < length; i++)
+                list.Add(rnd.Next(0, 100));
+            return list;
+        }
+    }
+}
---
+++ Task_21_02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_21_02
+{
+    // Дан текст. Написать метод, который возвращает словарь,
+    // где ключ — слово, а значение — количество его вхождений в тексте.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Считываем пользовательский текст как строку и разделяем на отдельные слова
+            Console.WriteLine("Напишите текст: ");
+            string[] words = Console.ReadLine().Split(",.;:!? ".ToCharArray());
+
+            Dictionary<string, int> dict = new();
+            foreach(string word in words)
+            {
+                if (",.;:!? ".Contains(word)) continue;
+                if (!dict.ContainsKey(word)) dict[word] = 1;
+                else dict[word]++;
+            }
+
+            Console.WriteLine($"Получившийся словарь: \n{string.Join('\n', dict)}");
+        }
+    }
+}
